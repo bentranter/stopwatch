cf492e51bc00520a632936e2cda81409dc421263
Ben Tranter <bwtrante@lakeheadu.ca>
2015-02-02 20:09:54 -0500

    First commit

--- NavBars/ViewController.swift
+++ NavBars/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var count = 0
+    
+    func updateTime() {
+        
+        count++
+        time.text = "\(count)"
+    }
+    
+    @IBOutlet var time: UILabel!
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        // Calls function resut every second
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer.invalidate()
+        count = 0
+        time.text = "\(count)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
